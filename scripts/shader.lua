4d440dbaf89efac848318722008b66fc7093c9f5
TheLeerName <lesha.nechaev.05@mail.ru>
2023-06-15 20:22:04 +0700

    upgraded shader script
    now supports applying shaders on FlxG.game!

--- scripts/shader.lua
+++ scripts/shader.lua
@@ -1,7 +1,7 @@
 -- WARNING: USES PSYCH ENGINE 0.6.3 AND LATER
 
 -- Author: TheLeerName
--- Description: script for applying shader to camHUD/camGame
+-- Description: script for applying shader to camHUD/camGame/FlxG.game
 -- How to use:
 --   1. Put this script in mods/scripts
 --   2. Change variables below
@@ -9,26 +9,51 @@
 local shaderName = "VHS2015" -- mods/shaders/VHS2015.frag
 local onCamHUD = true
 local onCamGame = false
+local onEntireGame = false
 
 -------------------------------- config ended --------------------------------
 ---------- dont touch anything below if you dont know what u doing! ----------
 
+local iTime_exists = false
 function onCreatePost()
 	shaderCoordFix()
 	makeLuaSprite("doritos")
 	makeGraphic("doritos", screenWidth, screenHeight)
-	runHaxeCode([[
-		game.initLuaShader(']]..shaderName..[[');
-		shader0 = game.createRuntimeShader(']]..shaderName..[[');
+	addHaxeLibrary("Type")
+	iTime_exists = runHaxeCode([[
+		if (!ClientPrefs.shaders) return false; // remove it if you want adding shaders with disabled option of it >:)
+
+		var shaderContent = [null, null];
+		var iTime_exists = false;
+		if (Type.resolveClass('sys.FileSystem').exists('mods/shaders/]]..shaderName..[[.frag')) {
+			shaderContent[0] = Type.resolveClass('sys.io.File').getContent('mods/shaders/]]..shaderName..[[.frag');
+			iTime_exists = shaderContent.contains('uniform float iTime;');
+		}
+		if (Type.resolveClass('sys.FileSystem').exists('mods/shaders/]]..shaderName..[[.vert'))
+			shaderContent[1] = Type.resolveClass('sys.io.File').getContent('mods/shaders/]]..shaderName..[[.vert');
+
+		shader0 = new FlxRuntimeShader(shaderContent[0], shaderContent[1]);
+		game.runtimeShaders.set(']]..shaderName..[[', shaderContent);
+
 		game.getLuaObject("doritos").shader = shader0;
 		if (]]..tostring(onCamHUD)..[[) game.camHUD.bgColor = 0x01000000;
-		if (]]..tostring(onCamHUD)..[[) game.camHUD.setFilters([new ShaderFilter(shader0)]);
-		if (]]..tostring(onCamGame)..[[) game.camGame.setFilters([new ShaderFilter(shader0)]);
+		if (]]..tostring(onCamHUD)..[[) game.camHUD.setFilters([new ShaderFilter(game.getLuaObject("doritos").shader)]);
+		if (]]..tostring(onCamGame)..[[) game.camGame.setFilters([new ShaderFilter(game.getLuaObject("doritos").shader)]);
+		if (]]..tostring(onEntireGame)..[[) FlxG.game.setFilters([new ShaderFilter(game.getLuaObject("doritos").shader)]);
+
+		return iTime_exists;
 	]])
+	-- example to set uniforms
+	-- setShaderFloat('doritos', 'prob', 1)
+end
+function onDestroy()
+	runHaxeCode("FlxG.game.setFilters([]);")
 end
 
 function onUpdatePost(elapsed)
-	setShaderFloat('doritos', 'iTime', os.clock())
+	if iTime_exists then
+		setShaderFloat('doritos', 'iTime', os.clock())
+	end
 end
 
 function shaderCoordFix()
